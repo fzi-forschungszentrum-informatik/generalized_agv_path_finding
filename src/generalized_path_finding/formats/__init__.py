4476660f6cd6fabba69a66beb4ab9897abb03fb5
Dominik Stampa <stampa@fzi.de>
2026-02-10 20:27:13 +0100

    another osm removal (experimental)

--- src/generalized_path_finding/formats/__init__.py
+++ src/generalized_path_finding/formats/__init__.py
@@ -3,6 +3,6 @@ from auto_all import start_all, end_all
 start_all()
 from .lif import LifDataProvider
 from .mfn_excel import MfnDataProvider
-from .osm import OsmDataProvider
+# from .osm import OsmDataProvider
 
 end_all()
